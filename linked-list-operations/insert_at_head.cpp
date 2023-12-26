045385f0a69f798e9aae302aacc1764136e2a1ba
BayajidAlam <bayajidalam2001@gmail.com>
2023-12-26 17:02:46 +0600

    insert at head done

--- linked-list-operations/insert_at_head.cpp
+++ linked-list-operations/insert_at_head.cpp
@@ -33,6 +33,7 @@ void insert_at_tail(Node *&head, int val)
   Node *tmp = head;
   while (tmp->next != NULL)
   {
+    // iterate till the node is before last node
     tmp = tmp->next;
   }
   // tmp is now in last node
@@ -44,10 +45,13 @@ void insert_at_tail(Node *&head, int val)
 // Insert at any position
 void insert_at_position(Node *head, int pos, int val)
 {
+  // create a new node
   Node *new_node = new Node(val);
+  // create a temp node like as head
   Node *tmp = head;
   for (int i = 1; i < pos - 1; i++)
   {
+    // locate the position - 1 node
     tmp = tmp->next;
   }
   // connect next node from tmp.next to new_node.next
@@ -56,6 +60,19 @@ void insert_at_position(Node *head, int pos, int val)
   new_node->next = tmp->next;
   tmp->next = new_node;
   cout << "Inserted at position: " << pos << endl
+       << endl;
+}
+
+// Insert at head
+void insert_at_head(Node *&head, int val)
+{
+  // first create a new node
+  Node *new_node = new Node(val);
+  // connect the head to the next of the new node
+  new_node->next = head;
+  // now the new node is the head
+  head = new_node;
+  cout << "Inserted at head" << endl
        << endl;
 }
 
@@ -84,7 +101,8 @@ int main()
     cout << "Option 1: Insert at tail" << endl;
     cout << "Option 2: Print link list" << endl;
     cout << "Option 3: Insert at any position" << endl;
-    cout << "Option 4: Terminate" << endl;
+    cout << "Option 4: Insert at head" << endl;
+    cout << "Option 5: Terminate" << endl;
 
     // taka the input as option
     int opt;
@@ -108,9 +126,20 @@ int main()
       cin >> pos;
       cout << "Please enter value: ";
       cin >> val;
-      insert_at_position(head,pos, val);
+      if (pos == 0)
+      {
+        insert_at_head(head, val);
+      }
+      insert_at_position(head, pos, val);
     }
     else if (opt == 4)
+    {
+      int val;
+      cout << "Please enter value: ";
+      cin >> val;
+      insert_at_head(head, val);
+    }
+    else if (opt == 5)
     {
       break;
     }
