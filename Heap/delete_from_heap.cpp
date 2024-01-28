fd786c8591c78d4fb1b9b9d9eebc045aa3c253aa
BayajidAlam <bayajidalam2001@gmail.com>
2024-01-28 23:11:11 +0600

    heap instruction added

--- Heap/delete_from_heap.cpp
+++ Heap/delete_from_heap.cpp
@@ -8,7 +8,7 @@ void insert_heap(vector<int> &v, int x)
   while (cur_idx != 0)
   {
     int parent_idx = (cur_idx - 1) / 2;
-    if (v[parent_idx] > v[cur_idx])
+    if (v[parent_idx] < v[cur_idx])
       swap(v[parent_idx], v[cur_idx]);
     else
       break;
@@ -26,14 +26,16 @@ void delete_heap(vector<int> &v)
     int left_idx = cur_idx * 2 + 1;
     int right_idx = cur_idx * 2 + 2;
     int last_idx = v.size() - 1;
-
+    // duita e ace
     if (left_idx <= last_idx && right_idx <= last_idx)
     {
+      // if left is greater than cur
       if (v[left_idx] >= v[right_idx] && v[left_idx] > v[cur_idx])
       {
         swap(v[left_idx], v[cur_idx]);
         cur_idx = left_idx;
       }
+      // if right is greater than cur
       else if (v[right_idx] >= v[left_idx] && v[right_idx] > v[cur_idx])
       {
         swap(v[right_idx], v[cur_idx]);
@@ -44,6 +46,7 @@ void delete_heap(vector<int> &v)
         break;
       }
     }
+    // left ace
     else if (left_idx <= last_idx)
     {
       if (v[left_idx] > v[cur_idx])
@@ -56,6 +59,7 @@ void delete_heap(vector<int> &v)
         break;
       }
     }
+    // right ace
     else if (right_idx <= last_idx)
     {
       if (v[right_idx] > v[cur_idx])
@@ -75,6 +79,13 @@ void delete_heap(vector<int> &v)
   }
 }
 
+void print_heap(vector<int> v)
+{
+  for (int val : v)
+    cout << val << " ";
+  cout << endl;
+}
+
 int main()
 {
   int n;
@@ -87,7 +98,6 @@ int main()
     insert_heap(v, x);
   }
   delete_heap(v);
-  for (int val : v)
-    cout << val << " ";
+  print_heap(v);
   return 0;
 }
--- Heap/insert_in_max_heap.cpp
+++ Heap/insert_in_max_heap.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 int main()
 {
-
-  vector<int> v;
   int n;
   cin >> n;
+  vector<int> v;
   for (int i = 0; i < n; i++)
   {
     int x;
     cin >> x;
     v.push_back(x);
     int cur_idx = v.size() - 1;
+    // child left: (i * 2)+1;
+    // child right: (i * 2)+2;
+    // parent: (i-1)/2
     while (cur_idx != 0)
     {
       int parent_idx = (cur_idx - 1) / 2;
